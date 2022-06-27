b22f9d6057c793640bd25c45c1f1ee896bd202ff
matiasfeliu92 <matumazparrote@gmail.com>
2022-06-27 18:51:38 -0300

    agregado de ruta para obtener una vista de SQL

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const control = require('../controllers/control')
 
 router.get('/lista', control.getAll)
+router.get('/view1', control.getView1)
+router.get('/view_1', control.getView_1)
 router.post('/insertar', control.insert)
 router.post('/editar/:id', control.update)
 
