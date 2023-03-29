457705f3e7ec6e36065e08edee5d492c6655bec8
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-29 12:43:28 -0300

    refactor: add router using rest-framework Router for urls api

--- provinciasCrud/urls.py
+++ provinciasCrud/urls.py
@@ -1,24 +1,9 @@
-# from rest_framework import routers
-# from provinciasCrud.api import ProvinciasViewSet
+from rest_framework import routers
+from provinciasCrud.views import ProvincesViewSet, RegionViewSet
 
-# router = routers.DefaultRouter()
+router = routers.DefaultRouter()
 
-# router.register('api/provincias', ProvinciasViewSet, 'provincias')
+router.register('provinces', ProvincesViewSet, 'provinces_routes')
+router.register('region', RegionViewSet, 'region_routes')
 
-# urlpatterns = router.urls
-
-from django.urls import path
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-from . import views
-
-urlpatterns = [
-    path('provinces', views.get_provinces, name='get_all_provinces'),
-    path('province/<int:id>', views.get_province, name='get_one_province'),
-    path('new_province', views.post_new_province, name='post_new_province'),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = router.urls
