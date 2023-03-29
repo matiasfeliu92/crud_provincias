bd6e06fb98d7e2a774f8ddece593334d9b5191b5
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-29 12:42:27 -0300

    refactor: add serializer for Provinces and Region models with their selected fields

--- provinciasCrud/serializers.py
+++ provinciasCrud/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Provinces
+from .models import Provinces, Region
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ('id', 'name')
 
 class ProvincesSerializer(serializers.ModelSerializer):
+    region_id = RegionSerializer()
     class Meta:
         model = Provinces
-        fields = ('id', 'name', 'surface', 'region')
+        fields = ('id', 'name', 'surface', 'region_id')
