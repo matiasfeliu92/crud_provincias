c29d27355535784b8c3814d6a66e501c1960dfef
matiasfeliu92 <matumazparrote@gmail.com>
2023-04-12 10:02:09 -0300

    delete files

--- server/provinciasCrud/api.py
+++
@@ -1,8 +0,0 @@
-from .models import Provinces
-from rest_framework import viewsets, permissions
-from .serializers import ProvincesSerializer
-
-class ProvincesViewSet(viewsets.ModelViewSet):
-    queryset = Provinces.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = ProvincesSerializer
