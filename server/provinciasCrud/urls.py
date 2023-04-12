fd2aa7eb0ab466f05f851082d79bf13954fcec48
matiasfeliu92 <matumazparrote@gmail.com>
2023-04-11 22:09:08 -0300

    refactor(back): update urls files for provinces and provincesCrud app

--- server/provinciasCrud/urls.py
+++ server/provinciasCrud/urls.py
@@ -14,11 +14,11 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('/', views.get_provinces, name='get_provinces'),
-    path('/cities', views.get_cities, name='get_cities'),
-    path('/regions', views.get_regions, name='get_regions'),
-    path('/<int:id>', views.get_one_province, name='get_one_province'),
-    path('/cities/<int:id>', views.get_one_city, name='get_one_city'),
-    path('/regions/<int:id>', views.get_one_region, name='get_one_region'),
-    path('/new_prov', views.post_new_province, name='post_new_province'),
+    path('', views.get_provinces, name='get_provinces'),
+    path('cities', views.get_cities, name='get_cities'),
+    path('regions', views.get_regions, name='get_regions'),
+    path('<int:id>', views.get_one_province, name='get_one_province'),
+    path('cities/<int:id>', views.get_one_city, name='get_one_city'),
+    path('regions/<int:id>', views.get_one_region, name='get_one_region'),
+    path('new_prov', views.post_new_province, name='post_new_province'),
 ]
