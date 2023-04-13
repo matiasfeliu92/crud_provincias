a42d5128455cfce76e9aa241da37a1b68030c1f7
matiasfeliu92 <matumazparrote@gmail.com>
2023-04-13 18:19:48 -0300

    refactor(back): update test.py file for make test to get one province by id and all provinces

--- server/provinciasCrud/tests.py
+++ server/provinciasCrud/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls.base import reverse
+
+from .models import Provinces
 
 # Create your tests here.
+class ProvincesModelTests(TestCase):
+    def test_get_one_province(self):
+        """if not province exist with passed id, return appropiate message"""
+        province = Provinces.objects.create(id=1, name='Santa Fe', population=23142323, density=5.8, surface=3252352)
+        response = self.client.get(reverse('provinciasCrud:get_one_province', args=[province.id]))
+        print(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Santa Fe')
+        # self.assertQuerysetEqual(response.context['province'], {})
+
+    def test_get_all_provinces(self):
+        """if provinces array is empty, return appropiate message"""
+        province = Provinces.objects.create(id=1, name='Santa Fe', population=23142323, density=5.8, surface=3252352)
+        response = self.client.get(reverse('provinciasCrud:get_provinces'))
+        print(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Santa Fe')
